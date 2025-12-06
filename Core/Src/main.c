f877d7a3f36c25de46bc5026e517187f29a4cdb8
Your Name <you@example.com>
2025-12-07 01:03:37 +0800

    [bsp][uart] Add USART3 Interrupt Configuration

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -87,7 +87,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-
+  MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
   yy_uart_init();
   printf("test printf123\r\n");
